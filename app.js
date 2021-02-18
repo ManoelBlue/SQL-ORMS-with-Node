a95d638973b2958360ea42ce1bfa3d40b91c6e81
Manoel Cunha <cunha@cngroup.dk>
2021-02-18 12:37:47 +0100

    test the connection

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'movies.db'
 });
+
+//async IIFE
+(async () => {
+    try {
+        // returns a promise that resolves to a successful,
+        // authenticated connection to the database:
+        await sequelize.authenticate();
+        console.log('Connection to the database successful!');
+    } catch (error) {
+        console.error('Error connecting to the database: ', error);
+    }
+}) ();
