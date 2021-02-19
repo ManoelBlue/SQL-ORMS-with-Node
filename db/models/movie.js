02f495469cb744e606faa58f7b20ccc9cb1a32f0
Manoel Cunha <cunha@cngroup.dk>
2021-02-19 10:07:16 +0100

    add new attributes to movie model

--- db/models/movie.js
+++ db/models/movie.js
@@ -3,7 +3,23 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
     class Movie extends Sequelize.Model {}
     Movie.init({
-        title: Sequelize.STRING,
+        title: {
+            type: Sequelize.STRING
+            // by default 255 length
+            // to change: Sequelize.STRING(500)
+        },
+        runtime: {
+            type: Sequelize.INTEGER
+        },
+        releaseDate: {
+            type: Sequelize.DATEONLY
+            // Obs.:
+            // type DATE => yyyy-mm-dd hh:mm:ss format
+            // type DATEONLY => yyyy-mm-dd
+        },
+        isAvailableOnVHS: {
+            type: Sequelize.BOOLEAN
+        },
     }, { sequelize });
 
     return Movie;
