4947ec51c18b58bcdab8197aadf7d8470f636c38
Manoel Cunha <cunha@cngroup.dk>
2021-02-19 10:42:17 +0100

    add validators: notEmpty and notNull

--- db/models/movie.js
+++ db/models/movie.js
@@ -14,10 +14,25 @@ module.exports = (sequelize) => {
             // by default 255 length
             // to change: Sequelize.STRING(500)
             allowNull: false, // disallow null
+            validate: {
+                // notEmpty: true,
+                // with a msg:
+                notEmpty: {
+                    msg: 'Please provide a value for "title"',
+                },
+                notNull: {
+                    msg: 'Please provide a value for "title"',
+                },
+            },
         },
         runtime: {
             type: Sequelize.INTEGER,
             allowNull: false, // disallow null
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "title"',
+                },
+            },
         },
         releaseDate: {
             type: Sequelize.DATEONLY,
@@ -25,11 +40,21 @@ module.exports = (sequelize) => {
             // type DATE => yyyy-mm-dd hh:mm:ss format
             // type DATEONLY => yyyy-mm-dd
             allowNull: false, // disallow null
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "title"',
+                },
+            },
         },
         isAvailableOnVHS: {
             type: Sequelize.BOOLEAN,
             allowNull: false, // disallow null
             defaultValue: false, // set default value
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "title"',
+                },
+            },
         },
     }, { sequelize });
 
